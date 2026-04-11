d3b7cceced2065c3e1e3c994c997fb09e5a96fee
JASH MANDALIYA <167299436+Jashmandaliya18@users.noreply.github.com>
2026-04-11 22:43:00 +0530

    Implement signup functionality with validation and password hashing

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,38 @@
+import { User } from "../models/user.model.js"
+import bcrypt from "bcryptjs"
+
+const signup = async (req, res) => {
+    try {
+        const { fullName, email, password, mobileNumber, role } = req.body;
+
+        if (!fullName || !email || !password || !mobileNumber || !role) {
+            return res.status().json({ message: "Provide All Details." });
+        }
+        const user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({ message: "User Already Exist with this Email" });
+        }
+        if (password.length() < 6) {
+            return res.status(400).json({ message: "Password must be at least 6 Characters" });
+        }
+        if (mobileNumber.length() < 10) {
+            return res.status(400).josn({ message: "Mobile must be at least 10 digits" });
+        }
+        const saltRounds = parseInt(process.env.SALT_ROUNDS) || 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+        user = await User.create({
+            fullName,
+            email,
+            password: hashedPassword,
+            mobileNumber,
+            role,
+        });
+
+
+    } catch (error) {
+
+    }
+
+
+}
