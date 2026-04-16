511c22e1085e8ff44b1c8b18a57be5bcc2965304
JASH MANDALIYA <167299436+Jashmandaliya18@users.noreply.github.com>
2026-04-16 13:09:44 +0530

    feat: add Cloudinary and Multer for file upload handling; implement uploadCloudinary function and configure multer storage

---
+++ backend/config/cloudinaty.js
@@ -0,0 +1,20 @@
+import { v2 as cloudinary } from 'cloudinary'
+import fs, { unlinkSync } from 'fs'
+
+const uploadCloudinary = async (file) => {
+    cloudinary.config({
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_API_SECRET
+    });
+    try {
+        const result = cloudinary.uploader.upload(file);
+        await fs.unlinkSync(file);
+        return result.secure_url;
+    } catch (error) {
+        await fs.unlinkSync(file);
+        console.log(error);
+    }
+}
+export default uploadCloudinary;
+
